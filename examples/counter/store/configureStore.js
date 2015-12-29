5be9cf1c60078ed3c4f71f35307fd4dab9079f60
Zalmoxisus <zalmoxisus@gmail.com>
2015-12-29 18:27:41 +0200

    Add examples

---
+++ examples/counter/store/configureStore.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import invariant from 'redux-immutable-state-invariant';
+import reducer from '../reducers';
+
+export default function configureStore(initialState) {
+  const finalCreateStore = compose(
+    applyMiddleware(invariant(), thunk)
+  )(createStore);
+
+  const store = finalCreateStore(reducer, initialState);
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
