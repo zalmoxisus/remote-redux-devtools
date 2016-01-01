eb18fc49e1f083a2330939af52da349b862f8df1
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-01 20:38:31 +0200

    Implement examples

--- examples/counter/store/configureStore.js
+++ examples/counter/store/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import invariant from 'redux-immutable-state-invariant';
+import devTools from 'remote-redux-devtools';
 import reducer from '../reducers';
 
 export default function configureStore(initialState) {
   const finalCreateStore = compose(
-    applyMiddleware(invariant(), thunk)
+    applyMiddleware(invariant(), thunk),
+    devTools()
   )(createStore);
 
   const store = finalCreateStore(reducer, initialState);
--- examples/todomvc/store/configureStore.js
+++ examples/todomvc/store/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore } from 'redux';
+import devTools from 'remote-redux-devtools';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = devTools()(createStore)(rootReducer, initialState);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
