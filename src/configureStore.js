e718ae70e099ca23d27c4ba6a5291a0d7f938fd0
Zalmoxisus <zalmoxisus@gmail.com>
2016-01-01 20:27:02 +0200

    Do not depend on React

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,5 +1,5 @@
 import { compose } from 'redux';
-import { instrument } from 'redux-devtools';
+import instrument from 'redux-devtools/lib/instrument';
 
 export default function configureStore(next, subscriber = () => ({})) {
   return compose(
