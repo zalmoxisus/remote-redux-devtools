f470610325aeeae0b5a663e668b6a347af264821
Jhen <developer@jhen.me>
2016-12-09 23:54:37 +0800

    Fix getHostForRN for newer React Native versions

--- src/utils/reactNative.js
+++ src/utils/reactNative.js
@@ -3,18 +3,19 @@
  * On Android emulator, the IP of host is `10.0.2.2` (Genymotion: 10.0.3.2)
  */
 export function getHostForRN(hostname) {
+  const { remoteModuleConfig } = typeof window !== 'undefined' &&
+    window.__fbBatchedBridgeConfig || {};
   if (
-    (hostname === 'localhost' || hostname === '127.0.0.1') &&
-    typeof window !== 'undefined' &&
-    window.__fbBatchedBridge &&
-    window.__fbBatchedBridge.RemoteModules &&
-    window.__fbBatchedBridge.RemoteModules.AndroidConstants
-  ) {
-    const {
-      ServerHost = hostname
-    } = window.__fbBatchedBridge.RemoteModules.AndroidConstants;
+    hostname !== 'localhost' && hostname !== '127.0.0.1' ||
+    !Array.isArray(remoteModuleConfig)
+  ) return hostname;
+
+  const [, AndroidConstants] = remoteModuleConfig.find(config =>
+    config && config[0] === 'AndroidConstants'
+  ) || [];
+  if (AndroidConstants) {
+    const { ServerHost = hostname } = AndroidConstants;
     return ServerHost.split(':')[0];
   }
-
   return hostname;
 }
